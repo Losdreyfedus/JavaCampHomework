2ead5a4eac962687f14a32f16116999bf6eeca82
Losdreyfedus <hamzakar13y@gmail.com>
2022-09-23 16:20:50 +0300

    Prime Number

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = -1;
+        boolean asalMi = false;
+        if (number == 1 || number<0){
+            System.out.println("Asal Değildir!");
+            return;
+        }
+        if (number == 0){
+            System.out.println("Geçersiz numara");
+            return;
+        }
+
+        for (int i = 2; i<number;i++){
+            if(number%i == 0){
+                asalMi = true;
+            }
+        }
+        if (asalMi == true){
+            System.out.println("Asal Değildir!");
+        }
+        else{
+            System.out.println("Asaldır!");
+        }
+    }
+}
