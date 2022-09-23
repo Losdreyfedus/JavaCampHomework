10bebeed429a821603401327adc9309dc76c0d9b
Hamza Kar <114229354+Losdreyfedus@users.noreply.github.com>
2022-09-23 16:25:12 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int number = -1;
+        int number = -1; //istediğiniz bir numaranın asal mı değil mi olduğunu gösteren bir proje.
         boolean asalMi = false;
         if (number == 1 || number<0){
             System.out.println("Asal Değildir!");
